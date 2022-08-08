2969e53772ffbffd17161988c66d8b3b96f9f5ea
g00u00 <g00u00@outlook.com>
2022-08-08 19:50:12 +0300

    new /info

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import App from "./App";
 import Header from "./Header";
 import Navigation from "./Navigation";
 import ContentItem from "./ContentItem";
 import Contents from "./Contents";
 import Footer from "./Footer";
 import Inputs from "./Inputs";
+import Object_0 from "./Object_0";
 
 function Container() {
     return (
         <div className="container">
             <Header/>
             <Navigation/>
-{/*            <ContentItem/>
-            <ContentsNav/>
-*/}
             <Contents  />
             <Inputs />
             <Footer/>
+            <Object_0/>
+            <ContentItem/>
+
         </div>
     );
 }
--- src/Object_0.tsx
+++ src/Object_0.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {objects} from "./objects"
 import {Object} from "./Object"
 
-export default function Contents() {
+export default function Object_0() {
     return (
         <div>
             <Object record={objects[0]} />
